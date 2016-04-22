2ab8c6965f3598aedead18363daaf88f4fa9c145
Cameron Heskett <cameron517@gmail.com>
2016-04-22 10:23:19 -0400

    Added create page

--- punchstarter/__init__.py
+++ punchstarter/__init__.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+import ipdb
+from flask import Flask, render_template, redirect, url_for, request
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
+import datetime
 
 app = Flask(__name__)
 app.config.from_object("punchstarter.default_settings")
@@ -16,3 +18,31 @@ from punchstarter.models import *
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+@app.route("/projects/create", methods=["GET","POST"])
+def create():
+    if request.method == "GET":
+        return render_template("create.html")
+    if request.method == "POST":
+        #Form submission
+
+        now = datetime.datetime.now()
+
+        time_end = request.form.get("funding_end_date")
+        time_end = datetime.datetime.strptime(time_end, "%Y-%m-%d")
+        
+        new_project = Project(
+            member_id = 1,
+        	name = request.form.get("project_name"),
+        	short_description =request.form.get("short_description"),
+        	long_description = request.form.get("long_description"),
+        	goal_amount = request.form.get("goal_amount"),
+        	time_start = now,
+        	time_end = time_end,
+        	time_created = now
+        )
+
+        db.session.add(new_project)
+        db.session.commit()
+
+        return redirect(url_for("create"))
--- punchstarter/default_settings.py
+++ punchstarter/default_settings.py
@@ -2,3 +2,4 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + BASE_DIR + "/app.db"
+DEBUG = True
